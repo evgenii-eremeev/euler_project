3249bb1a4f670f3cd1d1c02bdb9cbd16b45c0543
JayCrypto <jaycrypto@gmail.com>
2015-10-24 23:07:01 +0500

    made pip8 compliant

--- 1. Multiples of 3 and 5.py
+++ 1. Multiples of 3 and 5.py
@@ -1,12 +1,14 @@
 """
+https://projecteuler.net/problem=1
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-def sumDivBy(num, upto=1000):
-    n = upto // num
+
+def sum_div_by(num, limit=1000):
+    n = limit // num
     return n * (n + 1) / 2 * num
 
 # answer
-print(sumDivBy(3) + sumDivBy(5) - sumDivBy(15))
+print(sum_div_by(3) + sum_div_by(5) - sum_div_by(15))
